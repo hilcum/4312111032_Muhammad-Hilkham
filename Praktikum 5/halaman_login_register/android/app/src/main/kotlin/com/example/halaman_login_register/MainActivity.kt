fe57bfbbbf2e7a8047c9ffc7d34af858e52ef42e
unknown <muhammadhilkam9@gmail.com>
2023-09-21 21:13:16 +0700

    Praktikum ke 5

---
+++ Praktikum 5/halaman_login_register/android/app/src/main/kotlin/com/example/halaman_login_register/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.halaman_login_register
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Praktikum 5/navigator_push/android/app/src/main/kotlin/com/example/navigator_push/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.navigator_push
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
